c27eb3037f0f3354af2229d54b4e9c5ab16e17d7
Fredrik Noring <noring@nocrew.org>
2020-04-30 11:58:21 +0200

    toslibc: Add stdint.h and stddef.h

---
+++ include/toslibc/stddef.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#ifndef _TOSLIBC_STDDEF_H
+#define _TOSLIBC_STDDEF_H
+
+#include <stdint.h>
+
+#define NULL ((void *)0)
+
+typedef __SIZE_TYPE__ size_t;
+
+#endif /* _TOSLIBC_STDDEF_H */
