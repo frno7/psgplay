5ddd9f6bdb9d5b05b1f3af65bf147ceb43bb5c86
Fredrik Noring <noring@nocrew.org>
2020-12-26 21:47:04 +0100

    Define COUNT_ARGS macro

--- include/internal/macro.h
+++ include/internal/macro.h
@@ -17,6 +17,16 @@
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
+/* This counts to 32. Any more, it will return the 33rd argument. */
+#define __COUNT_ARGS(							\
+	 _0,  _1,  _2,  _3,  _4,  _5,  _6,  _7,  _8,  _9, _10,		\
+	_11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21,		\
+	_22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _n, X...) _n
+#define COUNT_ARGS(X...) __COUNT_ARGS(, ##X,				\
+	32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22,			\
+	21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11,			\
+	10,  9,  8,  7,  6,  5,  4,  3,  2,  1,  0)
+
 /* Indirect macros required for expanded argument pasting, eg. __LINE__. */
 #define ___PASTE(a,b) a##b
 #define __PASTE(a,b) ___PASTE(a,b)
