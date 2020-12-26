844e661c67d3c31d13fe0208f6f0975095e8cc29
Fredrik Noring <noring@nocrew.org>
2020-12-26 11:45:08 +0100

    Define XGEMDOS, the extended GEMDOS

---
+++ include/toslibc/tos/xgemdos-call.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: LGPL-2.1
+/*
+ * Copyright (C) 2020 Fredrik Noring
+ */
+
+#ifndef _TOSLIBC_TOS_XGEMDOS_CALL_H
+#define _TOSLIBC_TOS_XGEMDOS_CALL_H
+
+#define XGEMDOS_CALL(call)						\
+	call(0x00, void __NORETURN, reset,)				\
+	call(0x73, void, vdi, struct vdi *vdi_)				\
+	call(0xc8, void, aes, struct aes *aes_)				\
+	call(0xfe, int16_t, gdos_version,)
+
+#endif /* _TOSLIBC_TOS_XGEMDOS_CALL_H */
---
+++ include/toslibc/tos/xgemdos.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: LGPL-2.1
+/*
+ * Copyright (C) 2020 Fredrik Noring
+ */
+
+#ifndef _TOSLIBC_TOS_XGEMDOS_H
+#define _TOSLIBC_TOS_XGEMDOS_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "internal/macro.h"
+#include "internal/types.h"
+
+#include "xgemdos-call.h"
+
+#define XGEMDOS_DECALARE(opcode_, rtype_, name_, ...)			\
+rtype_ xgemdos_##name_(__VA_ARGS__);
+XGEMDOS_CALL(XGEMDOS_DECALARE)
+
+#endif /* _TOSLIBC_TOS_XGEMDOS_H */
