21f127ffb9f246957a55f2eb709d225ed3a41685
Fredrik Noring <noring@nocrew.org>
2020-12-27 10:12:39 +0100

    Remove unused m68020

--- lib/disassemble/m68k.c
+++ lib/disassemble/m68k.c
@@ -47,7 +47,6 @@ enum bfd_architecture
 #define bfd_mach_m68000 1
 #define bfd_mach_m68008 2
 #define bfd_mach_m68010 3
-#define bfd_mach_m68020 4
   bfd_arch_last
   };
 
@@ -135,11 +134,9 @@ typedef struct disassemble_info {
 #define	m68000   0x001
 #define	m68008   m68000 /* Synonym for -m68000.  otherwise unused.  */
 #define	m68010   0x002
-#define	m68020   0x004
 
 /* Handy aliases.  */
-#define	m68020up   (m68020)
-#define	m68010up   (m68010 | m68020up)
+#define	m68010up   (m68010)
 #define	m68000up   (m68000 | m68010up)
 
 /* The structure used to hold information for an opcode.  */
@@ -1804,9 +1801,6 @@ print_insn_m68k (bfd_vma memaddr, disassemble_info *info)
     case bfd_mach_m68010:
       arch_mask = m68010;
       break;
-    case bfd_mach_m68020:
-      arch_mask = m68020;
-      break;
     }
 
   fetch_data(info, buffer + 2);
@@ -1988,21 +1982,6 @@ const struct m68k_opcode m68k_opcodes[] =
 {"bgtw", 2,	one(0067000),	one(0177777), "BW", m68000up, m68k_insn_condbranch },
 {"blew", 2,	one(0067400),	one(0177777), "BW", m68000up, m68k_insn_condbranch },
 
-{"bhil", 2,	one(0061377),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"blsl", 2,	one(0061777),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"bccl", 2,	one(0062377),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"bcsl", 2,	one(0062777),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"bnel", 2,	one(0063377),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"beql", 2,	one(0063777),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"bvcl", 2,	one(0064377),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"bvsl", 2,	one(0064777),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"bpll", 2,	one(0065377),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"bmil", 2,	one(0065777),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"bgel", 2,	one(0066377),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"bltl", 2,	one(0066777),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"bgtl", 2,	one(0067377),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-{"blel", 2,	one(0067777),	one(0177777), "BL", m68020up, m68k_insn_condbranch },
-
 {"bhis", 2,	one(0061000),	one(0177400), "BB", m68000up, m68k_insn_condbranch },
 {"blss", 2,	one(0061400),	one(0177400), "BB", m68000up, m68k_insn_condbranch },
 {"bccs", 2,	one(0062000),	one(0177400), "BB", m68000up, m68k_insn_condbranch },
@@ -2039,45 +2018,19 @@ const struct m68k_opcode m68k_opcodes[] =
 {"bclr", 2,	one(0000600),	one(0170700), "Dd$s", m68000up },
 {"bclr", 4,	one(0004200),	one(0177700), "#b$s", m68000up },
 
-{"bfchg", 4,	two(0165300, 0), two(0177700, 0170000),	"?sO2O3",   m68020up },
-{"bfclr", 4,	two(0166300, 0), two(0177700, 0170000),	"?sO2O3",   m68020up },
-{"bfexts", 4,	two(0165700, 0), two(0177700, 0100000),	"/sO2O3D1", m68020up },
-{"bfextu", 4,	two(0164700, 0), two(0177700, 0100000),	"/sO2O3D1", m68020up },
-{"bfffo", 4,	two(0166700, 0), two(0177700, 0100000),	"/sO2O3D1", m68020up },
-{"bfins", 4,	two(0167700, 0), two(0177700, 0100000),	"D1?sO2O3", m68020up },
-{"bfset", 4,	two(0167300, 0), two(0177700, 0170000),	"?sO2O3",   m68020up },
-{"bftst", 4,	two(0164300, 0), two(0177700, 0170000),	"/sO2O3",   m68020up },
-
 {"bkpt", 2,	one(0044110),	one(0177770), "ts", m68010up },
 
 {"braw", 2,	one(0060000),	one(0177777), "BW", m68000up, m68k_insn_branch },
-{"bral", 2,	one(0060377),	one(0177777), "BL", m68020up, m68k_insn_branch},
 {"bras", 2,	one(0060000),	one(0177400), "BB", m68000up, m68k_insn_branch },
 
 {"bset", 2,	one(0000700),	one(0170700), "Dd$s", m68000up },
 {"bset", 4,	one(0004300),	one(0177700), "#b$s", m68000up },
 
 {"bsrw", 2,	one(0060400),	one(0177777), "BW", m68000up, m68k_insn_jsr },
-{"bsrl", 2,	one(0060777),	one(0177777), "BL", m68020up, m68k_insn_jsr},
 {"bsrs", 2,	one(0060400),	one(0177400), "BB", m68000up, m68k_insn_jsr },
 
 {"btst", 2,	one(0000400),	one(0170700), "Dd;b", m68000up },
 {"btst", 4,	one(0004000),	one(0177700), "#b@s", m68000up },
-
-{"callm", 4,	one(0003300),	one(0177700), "#b!s", m68020 },
-
-{"cas2w", 6,    two(0006374,0), two(0177777,0007070), "D3D6D2D5r1r4", m68020up },
-{"cas2w", 6,    two(0006374,0), two(0177777,0007070), "D3D6D2D5R1R4", m68020up },
-{"cas2l", 6,    two(0007374,0), two(0177777,0007070), "D3D6D2D5r1r4", m68020up },
-{"cas2l", 6,    two(0007374,0), two(0177777,0007070), "D3D6D2D5R1R4", m68020up },
-
-{"casb", 4,	two(0005300, 0), two(0177700, 0177070),	"D3D2~s", m68020up },
-{"casw", 4,	two(0006300, 0), two(0177700, 0177070),	"D3D2~s", m68020up },
-{"casl", 4,	two(0007300, 0), two(0177700, 0177070),	"D3D2~s", m68020up },
-
-{"chk2b", 4, 	two(0000300,0004000), two(0177700,07777), "!sR1", m68020up },
-{"chk2w", 4, 	two(0001300,0004000),	two(0177700,07777), "!sR1", m68020up },
-{"chk2l", 4, 	two(0002300,0004000),	two(0177700,07777), "!sR1", m68020up },
 
 {"chkl", 2,	one(0040400),		one(0170700), ";lDd", m68000up },
 {"chkw", 2,	one(0040600),		one(0170700), ";wDd", m68000up },
@@ -2085,10 +2038,6 @@ const struct m68k_opcode m68k_opcodes[] =
 {"clrb", 2,	one(0041000),	one(0177700), "$s", m68000up },
 {"clrw", 2,	one(0041100),	one(0177700), "$s", m68000up },
 {"clrl", 2,	one(0041200),	one(0177700), "$s", m68000up },
-
-{"cmp2b", 4,	two(0000300,0), two(0177700,07777), "!sR1", m68020up },
-{"cmp2w", 4,	two(0001300,0),	two(0177700,07777), "!sR1", m68020up },
-{"cmp2l", 4,	two(0002300,0),	two(0177700,07777), "!sR1", m68020up },
 
 {"cmpaw", 2,	one(0130300),	one(0170700), "*wAd", m68000up },
 {"cmpal", 2,	one(0130700),	one(0170700), "*lAd", m68000up },
@@ -2131,19 +2080,7 @@ const struct m68k_opcode m68k_opcodes[] =
 {"dbvc", 2,	one(0054310),	one(0177770), "DsBw", m68000up, m68k_insn_condbranch },
 {"dbvs", 2,	one(0054710),	one(0177770), "DsBw", m68000up, m68k_insn_condbranch },
 
-{"divsl", 4, 	two(0046100,0006000),two(0177700,0107770),";lD3D1", m68020up },
-{"divsl", 4, 	two(0046100,0004000),two(0177700,0107770),";lDD",   m68020up },
-
-{"divsll", 4, 	two(0046100,0004000),two(0177700,0107770),";lD3D1",m68020up },
-{"divsll", 4, 	two(0046100,0004000),two(0177700,0107770),";lDD",  m68020up },
-
 {"divuw", 2,	one(0100300),		one(0170700), ";wDd", m68000up },
-
-{"divul", 4,	two(0046100,0002000),two(0177700,0107770),";lD3D1", m68020up },
-{"divul", 4,	two(0046100,0000000),two(0177700,0107770),";lDD",   m68020up },
-
-{"divull", 4,	two(0046100,0000000),two(0177700,0107770),";lD3D1",m68020up },
-{"divull", 4,	two(0046100,0000000),two(0177700,0107770),";lDD",  m68020up },
 
 {"eorib", 4,	one(0005000),	one(0177700), "#b$s", m68000up },
 {"eorib", 4,	one(0005074),	one(0177777), "#bCs", m68000up },
@@ -2175,7 +2112,6 @@ const struct m68k_opcode m68k_opcodes[] =
 
 {"extw", 2,	one(0044200),	one(0177770), "Ds", m68000up },
 {"extl", 2,	one(0044300),	one(0177770), "Ds", m68000up },
-{"extbl", 2,	one(0044700),	one(0177770), "Ds", m68020up },
 
 {"illegal", 2,	one(0045374),	one(0177777), "",   m68000up },
 
@@ -2192,9 +2128,7 @@ const struct m68k_opcode m68k_opcodes[] =
 {"lea", 2,	one(0040700),	one(0170700), "!sAd", m68000up },
 
 {"linkw", 4,	one(0047120),	one(0177770), "As#w", m68000up },
-{"linkl", 6,	one(0044010),	one(0177770), "As#l", m68020up },
 {"link", 4,	one(0047120),	one(0177770), "As#W", m68000up },
-{"link", 6,	one(0044010),	one(0177770), "As#l", m68020up },
 
 {"lslb", 2,	one(0160410),	one(0170770), "QdDs", m68000up },
 {"lslb", 2,	one(0160450),	one(0170770), "DdDs", m68000up },
@@ -2270,12 +2204,8 @@ const struct m68k_opcode m68k_opcodes[] =
 {"movesl", 4,	two(0007200, 04000), two(0177700, 07777), "R1~s", m68010up },
 
 {"mulsw", 2,	one(0140700),		one(0170700), ";wDd", m68000up },
-{"mulsl", 4,	two(0046000,004000), two(0177700,0107770), ";lD1", m68020up },
-{"mulsl", 4,	two(0046000,006000), two(0177700,0107770), ";lD3D1",m68020up },
 
 {"muluw", 2,	one(0140300),		one(0170700), ";wDd", m68000up },
-{"mulul", 4,	two(0046000,000000), two(0177700,0107770), ";lD1", m68020up },
-{"mulul", 4,	two(0046000,002000), two(0177700,0107770), ";lD3D1",m68020up },
 
 {"nbcd", 2,	one(0044000),	one(0177700), "$s", m68000up },
 
@@ -2320,9 +2250,6 @@ const struct m68k_opcode m68k_opcodes[] =
 {"or", 2,	one(0100100),	one(0170700), ";wDd", m68000up },
 {"or", 2,	one(0100500),	one(0170700), "Dd~s", m68000up },
 
-{"pack", 4,	one(0100500),	one(0170770), "DsDd#w", m68020up },
-{"pack", 4,	one(0100510),	one(0170770), "-s-d#w", m68020up },
-
 {"pea", 2,	one(0044100),		one(0177700), "!s", m68000up },
 
 {"reset", 2,	one(0047160),		one(0177777), "", m68000up },
@@ -2362,8 +2289,6 @@ const struct m68k_opcode m68k_opcodes[] =
 {"rtd", 4,	one(0047164),		one(0177777), "#w", m68010up },
 
 {"rte", 2,	one(0047163),		one(0177777), "",   m68000up },
-
-{"rtm", 2,	one(0003300),		one(0177760), "Rs", m68020 },
 
 {"rtr", 2,	one(0047167),		one(0177777), "",   m68000up },
 
@@ -2440,71 +2365,13 @@ const struct m68k_opcode m68k_opcodes[] =
 
 {"trap", 2,	one(0047100),	one(0177760), "Ts", m68000up },
 
-{"trapcc", 2,	one(0052374),	one(0177777), "", m68020up },
-{"trapcs", 2,	one(0052774),	one(0177777), "", m68020up },
-{"trapeq", 2,	one(0053774),	one(0177777), "", m68020up },
-{"trapf", 2,	one(0050774),	one(0177777), "", m68020up },
-{"trapge", 2,	one(0056374),	one(0177777), "", m68020up },
-{"trapgt", 2,	one(0057374),	one(0177777), "", m68020up },
-{"traphi", 2,	one(0051374),	one(0177777), "", m68020up },
-{"traple", 2,	one(0057774),	one(0177777), "", m68020up },
-{"trapls", 2,	one(0051774),	one(0177777), "", m68020up },
-{"traplt", 2,	one(0056774),	one(0177777), "", m68020up },
-{"trapmi", 2,	one(0055774),	one(0177777), "", m68020up },
-{"trapne", 2,	one(0053374),	one(0177777), "", m68020up },
-{"trappl", 2,	one(0055374),	one(0177777), "", m68020up },
-{"trapt", 2,	one(0050374),	one(0177777), "", m68020up },
-{"trapvc", 2,	one(0054374),	one(0177777), "", m68020up },
-{"trapvs", 2,	one(0054774),	one(0177777), "", m68020up },
-
-{"trapccw", 4,	one(0052372),	one(0177777), "#w", m68020up },
-{"trapcsw", 4,	one(0052772),	one(0177777), "#w", m68020up },
-{"trapeqw", 4,	one(0053772),	one(0177777), "#w", m68020up },
-{"trapfw", 4,	one(0050772),	one(0177777), "#w", m68020up },
-{"trapgew", 4,	one(0056372),	one(0177777), "#w", m68020up },
-{"trapgtw", 4,	one(0057372),	one(0177777), "#w", m68020up },
-{"traphiw", 4,	one(0051372),	one(0177777), "#w", m68020up },
-{"traplew", 4,	one(0057772),	one(0177777), "#w", m68020up },
-{"traplsw", 4,	one(0051772),	one(0177777), "#w", m68020up },
-{"trapltw", 4,	one(0056772),	one(0177777), "#w", m68020up },
-{"trapmiw", 4,	one(0055772),	one(0177777), "#w", m68020up },
-{"trapnew", 4,	one(0053372),	one(0177777), "#w", m68020up },
-{"trapplw", 4,	one(0055372),	one(0177777), "#w", m68020up },
-{"traptw", 4,	one(0050372),	one(0177777), "#w", m68020up },
-{"trapvcw", 4,	one(0054372),	one(0177777), "#w", m68020up },
-{"trapvsw", 4,	one(0054772),	one(0177777), "#w", m68020up },
-
-{"trapccl", 6,	one(0052373),	one(0177777), "#l", m68020up },
-{"trapcsl", 6,	one(0052773),	one(0177777), "#l", m68020up },
-{"trapeql", 6,	one(0053773),	one(0177777), "#l", m68020up },
-{"trapfl", 6,	one(0050773),	one(0177777), "#l", m68020up },
-{"trapgel", 6,	one(0056373),	one(0177777), "#l", m68020up },
-{"trapgtl", 6,	one(0057373),	one(0177777), "#l", m68020up },
-{"traphil", 6,	one(0051373),	one(0177777), "#l", m68020up },
-{"traplel", 6,	one(0057773),	one(0177777), "#l", m68020up },
-{"traplsl", 6,	one(0051773),	one(0177777), "#l", m68020up },
-{"trapltl", 6,	one(0056773),	one(0177777), "#l", m68020up },
-{"trapmil", 6,	one(0055773),	one(0177777), "#l", m68020up },
-{"trapnel", 6,	one(0053373),	one(0177777), "#l", m68020up },
-{"trappll", 6,	one(0055373),	one(0177777), "#l", m68020up },
-{"traptl", 6,	one(0050373),	one(0177777), "#l", m68020up },
-{"trapvcl", 6,	one(0054373),	one(0177777), "#l", m68020up },
-{"trapvsl", 6,	one(0054773),	one(0177777), "#l", m68020up },
-
 {"trapv", 2,	one(0047166),	one(0177777), "", m68000up },
 
-{"tstb", 2,	one(0045000),	one(0177700), ";b", m68020up },
 {"tstb", 2,	one(0045000),	one(0177700), "$b", m68000up },
-{"tstw", 2,	one(0045100),	one(0177700), "*w", m68020up },
 {"tstw", 2,	one(0045100),	one(0177700), "$w", m68000up },
-{"tstl", 2,	one(0045200),	one(0177700), "*l", m68020up },
 {"tstl", 2,	one(0045200),	one(0177700), "$l", m68000up },
 
 {"unlk", 2,	one(0047130),	one(0177770), "As", m68000up },
-
-{"unpk", 4,	one(0100600),	one(0170770), "DsDd#w", m68020up },
-{"unpk", 4,	one(0100610),	one(0170770), "-s-d#w", m68020up },
-
 };
 
 const int m68k_numopcodes = sizeof m68k_opcodes / sizeof m68k_opcodes[0];
