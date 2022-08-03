8fd381f1ee44aca6420fa9ef04ad825ee5eaa24b
Fredrik Noring <noring@nocrew.org>
2022-08-03 16:56:10 +0200

    Replace the lib/toslibc directory with its Git repo as a submodule

--- lib/toslibc/system-variable.c
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: LGPL-2.1
-/*
- * Copyright (C) 2019 Fredrik Noring
- */
-
-#include <tos/system-variable.h>
-
-struct system_variables *__system_variables =
-	(struct system_variables *)0x400;
