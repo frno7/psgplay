aa9c644038d3209a9c8717f31898b636091f0652
Fredrik Noring <noring@nocrew.org>
2023-05-23 17:50:47 +0200

    Use psgplay_stop_at_time

--- system/unix/command-mode.c
+++ system/unix/command-mode.c
@@ -36,7 +36,7 @@ struct replay_state {
 	ssize_t sample_count;
 };
 
-static ssize_t parse_time(const char *s, int frequency)
+static ssize_t parse_time(const char *s)
 {
 	float a, b;
 	const int r = sscanf(s, "%f:%f", &a, &b);
@@ -44,36 +44,34 @@ static ssize_t parse_time(const char *s, int frequency)
 	if (!r)
 		pr_fatal_error("%s: malformed time '%s'\n", progname, s);
 
-	return roundf((r == 2 ? 60.0f * a + b : a) * frequency);
+	return roundf(r == 2 ? 60.0f * a + b : a);
 }
 
-static ssize_t parse_start(const char *s, int frequency)
+static float parse_start(const char *s)
 {
-	return !s ? 0 : parse_time(s, frequency);
+	return !s ? 0 : parse_time(s);
 }
 
-static ssize_t parse_stop_auto(int track, int frequency, struct file file)
+static float parse_stop_auto(int track, struct file file)
 {
 	float duration;
 
 	if (!sndh_tag_subtune_time(&duration, track, file.data, file.size))
 		return OPTION_TIME_UNDEFINED;
 
-	return duration > 0 ?  roundf(duration * frequency) : OPTION_STOP_NEVER;
+	return duration > 0 ? duration : OPTION_STOP_NEVER;
 }
 
-static ssize_t parse_stop(const char *s,
-	int track, int frequency, struct file file)
+static float parse_stop(const char *s, int track, struct file file)
 {
 	return !s                      ? OPTION_TIME_UNDEFINED :
-	       strcmp(s, "auto") == 0  ? parse_stop_auto(track, frequency, file) :
-	       strcmp(s, "never") == 0 ? OPTION_STOP_NEVER :
-					 parse_time(s, frequency);
+	       strcmp(s, "auto") == 0  ? parse_stop_auto(track, file) :
+	       strcmp(s, "never") == 0 ? OPTION_STOP_NEVER : parse_time(s);
 }
 
-static ssize_t parse_length(const char *s, int frequency, ssize_t start)
+static float parse_length(const char *s, float start)
 {
-	return !s ? OPTION_TIME_UNDEFINED : start + parse_time(s, frequency);
+	return !s ? OPTION_TIME_UNDEFINED : start + parse_time(s);
 }
 
 static ssize_t stop_or_length(ssize_t stop, ssize_t length)
@@ -88,13 +86,11 @@ void command_replay(const struct options *options, struct file file,
 {
 	const char *auto_stop = options->stop ? options->stop :
 		!options->stop && !options->length ? "auto" : NULL;
-	const ssize_t sample_start = parse_start(
-		options->start, options->frequency);
-	const ssize_t stop = parse_stop(
-		auto_stop, options->track, options->frequency, file);
-	const ssize_t length = parse_length(
-		options->length, options->frequency, sample_start);
-	const ssize_t sample_stop = stop_or_length(stop, length);
+	const float time_start = parse_start(options->start);
+	const ssize_t sample_start = time_start * options->frequency;
+	const float length = parse_length(options->length, time_start);
+	const float time_stop = stop_or_length(
+		parse_stop(auto_stop, options->track, file), length);
 	void *output_arg = output->open(
 		options->output, options->frequency, false);
 	struct psgplay *pp = psgplay_init(file.data, file.size,
@@ -105,6 +101,9 @@ void command_replay(const struct options *options, struct file file,
 		pr_fatal_error("%s: failed to init PSG play\n", progname);
 
 	psgplay_digital_to_stereo_callback(pp, psg_mix_option(), NULL);
+
+	if (time_stop >= 0)
+		psgplay_stop_at_time(pp, time_stop);
 
 	for (;;) {
 		struct psgplay_stereo buffer[256];
@@ -122,10 +121,6 @@ void command_replay(const struct options *options, struct file file,
 			}
 
 			sample_count++;
-
-			if (0 <= sample_stop &&
-				 sample_stop <= sample_count)
-				goto out;
 
 			if (!output->sample(
 					buffer[i].left,
