e8861e81793bcd30df9e213ccd9ff86e8862b55b
Fredrik Noring <noring@nocrew.org>
2025-08-23 14:54:08 +0200

    test: Verify 16 Hz tempo

---
+++ include/test/tempo16hz.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2025 Fredrik Noring
+ */
+
+#ifndef PSGPLAY_TEST_TEMPO16HZ_H
+#define PSGPLAY_TEST_TEMPO16HZ_H
+
+#define tempo16hz_tune_value_names(t)					\
+	t(0, "16 Hz timer D half period (8 Hz)")
+
+#endif /* PSGPLAY_TEST_TEMPO16HZ_H */
---
+++ test/tempo16hz.c
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <asm/sndh.h>
+
+#include "test/tempo.h"
+#include "test/tempo16hz.h"
+
+sndh_title("Tempo 16 Hz");
+sndh_tune_value_names(int, tempo16hz_tune_value_names);
+sndh_timer(SNDH_TIMER_D, 16);
+
+static struct tempo_sample sample[1];
+
+void sndh_init(int tune)
+{
+	tempo_init(sample);
+}
+
+void sndh_play()
+{
+	tempo_play(sample);
+}
+
+void sndh_exit()
+{
+	tempo_exit(sample);
+}
