11de19f95cfac1fc7c8467b3143e3e7fc5f4490f
Fredrik Noring <noring@nocrew.org>
2025-08-25 07:48:07 +0200

    test: Verify tempo

--- test/tempo-verify.c
+++ test/tempo-verify.c
@@ -2,9 +2,10 @@
 
 #include "toslibc/asm/machine.h"
 
+#include "atari/machine.h"
+
 #include "test/report.h"
 #include "test/verify.h"
-
 #include "test/tempo.h"
 
 test_value_names(struct timer_preset, tune_value_names);
@@ -13,6 +14,8 @@ static double timer_frequency(const struct options *options)
 {
 	const struct timer_preset preset = test_value(options);
 
+	if (!preset.ctrl && preset.frequency == 50)
+		return CPU_FREQUENCY / (double)ATARI_STE_CYCLES_PER_VBL_PAL;
 	if (!preset.ctrl)
 		return preset.frequency;
 
@@ -20,8 +23,6 @@ static double timer_frequency(const struct options *options)
 }
 
 #define INIT								\
-	const struct timer_preset preset = test_value(options);		\
-									\
 	/*								\
 	 * One interrupt is half a period, so				\
 	 * multiply with 0.5 and 2.0 accordingly.			\
@@ -29,17 +30,19 @@ static double timer_frequency(const struct options *options)
 	const struct test_wave_deviation wave_deviation =		\
 		test_wave_deviation(audio);				\
 	const struct test_wave_error error = test_wave_error(		\
-		audio->format, wave_deviation, 0.5 * timer_frequency(options)); \
-									\
-	const double interrupt_frequency = 2.0 *			\
-		audio_frequency_from_period(				\
-			wave_deviation.wave.period,			\
-			audio->format.frequency)
+		audio->format, wave_deviation,				\
+		0.5 * timer_frequency(options))
 
 void report(struct strbuf *sb, const struct audio *audio,
 	const struct options *options)
 {
 	INIT;
+
+	const struct timer_preset preset = test_value(options);
+	const double interrupt_frequency = 2.0 *
+		audio_frequency_from_period(
+			wave_deviation.wave.period,
+			audio->format.frequency);
 
 	report_input(sb, audio, test_name(options), options);
 
@@ -51,6 +54,12 @@ void report(struct strbuf *sb, const struct audio *audio,
 			ATARI_MFP_XTAL,
 			preset.divisor,
 			preset.count);
+	else if (preset.frequency == 50)
+		sbprintf(sb,
+			"cpu clock %d Hz\n"
+			"vbl count %d cycles\n",
+			CPU_FREQUENCY,
+			ATARI_STE_CYCLES_PER_VBL_PAL);
 
 	sbprintf(sb, "timer frequency %f Hz\n", timer_frequency(options));
 
@@ -67,7 +76,13 @@ void report(struct strbuf *sb, const struct audio *audio,
 
 char *verify(const struct audio *audio, const struct options *options)
 {
-	// FIXME
+	INIT;
+
+	verify_assert (wave_deviation.deviation.maximum <= 5.0)
+		return "wave deviation max";
+
+	verify_assert (error.relative_frequency <= 9e-7)
+		return "wave error relative frequency";
 
 	return NULL;
 }
