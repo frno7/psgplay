aeb252186b5e6bb6959232f1306674ae7bae4510
Fredrik Noring <noring@nocrew.org>
2026-03-15 13:11:32 +0100

    Declare struct psgplay in include/internal/psgplay.h

---
+++ include/internal/psgplay.h
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2026 Fredrik Noring
+ */
+
+#ifndef INTERNAL_PSGPLAY_H
+#define INTERNAL_PSGPLAY_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "psgplay/stereo.h"
+
+struct fir8 {
+	int16_t xn[8];
+	int k;
+};
+
+struct stereo_buffer {
+	size_t index;
+	size_t count;
+	size_t capacity;
+	size_t total;
+	struct psgplay_stereo *sample;
+};
+
+struct digital_buffer {
+	size_t index;
+	struct {
+		size_t psg;
+		size_t sound;
+		size_t mixer;
+	} count;
+	size_t capacity;
+	size_t total;
+	size_t stop;
+	struct psgplay_digital *sample;
+};
+
+struct psgplay {
+	struct stereo_buffer stereo_buffer;
+	struct digital_buffer digital_buffer;
+
+	struct {
+		uint64_t psg;
+		uint64_t dma;
+		uint64_t mix;
+		uint64_t play;
+	} record;
+
+	struct psgplay_downsample {
+		int stereo_frequency;
+		uint64_t psg_cycle;
+		uint64_t downsample_sample_cycle;
+
+		struct {
+			struct fir8 left;
+			struct fir8 right;
+		} lowpass;
+	} downsample;
+
+	struct {
+		psgplay_digital_to_stereo_cb cb;
+		void *arg;
+	} digital_to_stereo_callback;
+
+	struct {
+		psgplay_stereo_downsample_cb cb;
+		void *arg;
+	} stereo_downsample_callback;
+
+	const struct machine *machine;
+
+	struct {
+		void (*cb)(uint32_t pc, void *arg);
+		void *arg;
+	} instruction_callback;
+
+	int errno_;
+};
+
+#endif /* INTERNAL_PSGPLAY_H */
--- lib/psgplay/psgplay.c
+++ lib/psgplay/psgplay.c
@@ -11,6 +11,7 @@
 
 #include "internal/assert.h"
 #include "internal/compare.h"
+#include "internal/psgplay.h"
 
 #include "atari/cpu.h"
 #include "atari/dac.h"
@@ -28,74 +29,6 @@
 #define FADE_SAMPLES 2500	/* 10 ms with 250 kHz */
 
 #define RECORD_PLAY_DEFAULT ((10 * ATARI_STE_EXT_OSC) / 4 / 16)    /* 10 s */
-
-struct fir8 {
-	int16_t xn[8];
-	int k;
-};
-
-struct stereo_buffer {
-	size_t index;
-	size_t count;
-	size_t capacity;
-	size_t total;
-	struct psgplay_stereo *sample;
-};
-
-struct digital_buffer {
-	size_t index;
-	struct {
-		size_t psg;
-		size_t sound;
-		size_t mixer;
-	} count;
-	size_t capacity;
-	size_t total;
-	size_t stop;
-	struct psgplay_digital *sample;
-};
-
-struct psgplay {
-	struct stereo_buffer stereo_buffer;
-	struct digital_buffer digital_buffer;
-
-	struct {
-		uint64_t psg;
-		uint64_t dma;
-		uint64_t mix;
-		uint64_t play;
-	} record;
-
-	struct psgplay_downsample {
-		int stereo_frequency;
-		uint64_t psg_cycle;
-		uint64_t downsample_sample_cycle;
-
-		struct {
-			struct fir8 left;
-			struct fir8 right;
-		} lowpass;
-	} downsample;
-
-	struct {
-		psgplay_digital_to_stereo_cb cb;
-		void *arg;
-	} digital_to_stereo_callback;
-
-	struct {
-		psgplay_stereo_downsample_cb cb;
-		void *arg;
-	} stereo_downsample_callback;
-
-	const struct machine *machine;
-
-	struct {
-		void (*cb)(uint32_t pc, void *arg);
-		void *arg;
-	} instruction_callback;
-
-	int errno_;
-};
 
 static int buffer_stereo_sample(struct stereo_buffer *sb,
 	const struct psgplay_stereo *stereo, const size_t count)
